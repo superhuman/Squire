11d695b6eeeb739170c739151fb86df6943678b5
Neil Jenkins <neil@nmjenkins.com>
2017-09-05 11:42:54 +1000

    Better handling of <pre>
    * Pasting <pre> should not attempt to merge with block.
    * Hitting enter should produce a new <pre>, otherwise all new lines after the
      break are lost.

--- source/Editor.js
+++ source/Editor.js
@@ -1243,7 +1243,8 @@ proto.changeFormat = function ( add, remove, range, partial ) {
 var tagAfterSplit = {
     DT:  'DD',
     DD:  'DT',
-    LI:  'LI'
+    LI:  'LI',
+    PRE: 'PRE'
 };
 
 var splitBlock = function ( self, block, node, offset ) {
